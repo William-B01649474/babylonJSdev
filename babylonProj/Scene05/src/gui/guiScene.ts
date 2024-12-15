a3d0fb407be991c8947a3c62c0e5a4e21b3a0549
William-B01649474 <b01649474@studentmail.uws.ac.uk>
2024-12-15 00:46:06 +0000

    hh
    hhhhhh

--- babylonProj/Scene05/src/gui/guiScene.ts
+++ babylonProj/Scene05/src/gui/guiScene.ts
@@ -51,11 +51,11 @@ import {
     let button = GUI.Button.CreateSimpleButton(name, note);
         button.left = x;
         button.top = y;
-        button.width = "80px";
-        button.height = "30px";
+        button.width = "120px";
+        button.height = "40px";
         button.color = "white";
         button.cornerRadius = 20;
-        button.background = "purple";
+        button.background = "red";
 
         /*
         const buttonClick = new Sound("MenuClickSFX", "./audio/menu-click.wav", scene, null, {
@@ -86,7 +86,7 @@ import {
     camTarget,
     scene,
   );
-  camera.attachControl(true);
+  //camera.attachControl(true);
   return camera;
 }
   
@@ -99,7 +99,7 @@ import {
       button1: GUI.Button;
       button2: GUI.Button;
       button3: GUI.Button;
-      button4: GUI.Button;
+      //button4: GUI.Button;
       camera: Camera;
     }
   
@@ -108,10 +108,10 @@ import {
     //----------------------------------------------------------
     let scene = new Scene(engine);
     let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI", true);
-    var button1 = createSceneButton(scene,"but1", "1",1,"-150px", "120px", advancedTexture);
-    var button2 = createSceneButton(scene,"but2", "2", 2,"-50px", "120px", advancedTexture);
-    var button3 = createSceneButton(scene,"but3", "3",3,"50px", "120px", advancedTexture);
-    var button4 = createSceneButton(scene,"but4", "4", 4,"150px", "120px", advancedTexture);
+    var button1 = createSceneButton(scene,"but1", "PLAY",1,"0px", "-60px", advancedTexture);
+    var button2 = createSceneButton(scene,"but2", "OPTIONS", 2,"0px", "0px", advancedTexture);
+    var button3 = createSceneButton(scene,"but3", "QUIT",3,"0px", "60px", advancedTexture);
+    //var button4 = createSceneButton(scene,"but4", "4", 4,"0px", "120px", advancedTexture);
     var camera = createArcRotateCamera(scene);
 
  
@@ -121,7 +121,7 @@ import {
       button1,
       button2,
       button3,
-      button4,
+      //button4,
       camera
     };
     
